63529e3f5b83a59681b489386e96257dd40017eb
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-12 19:30:11 +0530

    feat: generate playground graphql resolver

--- apps/api/src/modules/playground/playground.module.ts
+++ apps/api/src/modules/playground/playground.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PlaygroundController } from './playground.controller';
 import { PlaygroundService } from './playground.service';
+import { PlaygroundResolver } from './playground.resolver';
 
 @Module({
   controllers: [PlaygroundController],
-  providers: [PlaygroundService]
+  providers: [PlaygroundService, PlaygroundResolver]
 })
 export class PlaygroundModule {}
